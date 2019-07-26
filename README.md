9ac3fa4b1d70349b02ed5a68964e30b18fb358c1
Fedor Iskhakov <fediskhakov@users.noreply.github.com>
2019-07-26 02:33:19 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# test-assignment
+Week1 assignment and Lab1 code
+
+Please see Jupyter Notebook assignment1.ipynb for instructions
---
+++ assignment1.py
@@ -0,0 +1,19 @@
+print('Hello world!')
+
+import sys
+print('Python ' + sys.version)
+print(sys.float_info)
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.random.randn(100)
+plt.plot(x)
+plt.show()
+
+# **The assignment:**
+# 1. Find the code from this notebook in _assingment1.py_ file.
+# 2. Modify the code in any way
+# 3. Commit the modified code to local repository
+# 4. Push changes to GitHub
+# 5. Create pull request (PR) to submit the assignment
